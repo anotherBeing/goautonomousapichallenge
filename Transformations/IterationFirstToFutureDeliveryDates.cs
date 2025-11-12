44ba0697de66a016935b67e8d78b965026d8a148
Jonatan Kornhill <jonakor@dfds.com>
2025-11-12 21:16:59 +0100

    Implement Challenge 1

---
+++ Transformations/IterationFirstToFutureDeliveryDates.cs
@@ -0,0 +1,42 @@
+﻿using TransformApi.Model;
+
+namespace TransformApi.Transformations;
+public class IterationFirstToFutureDeliveryDates : ITransformation<Payload>
+{
+    private const string TransformationTrigger = "first";
+
+    public Payload Transform(Payload payload)
+    {
+        if (payload.Iteration is null)
+            return payload;
+
+        var shouldTransform = payload.Iteration.Equals(TransformationTrigger, StringComparison.InvariantCultureIgnoreCase);
+
+        if (!shouldTransform)
+            return payload;
+
+        var today = DateTime.UtcNow.Date; //TODO: Should we use local time?
+
+        var updatedProducts = payload.Products?
+            .Select(product =>
+            {
+                if (product?.DeliveryDate is not null
+                    && DateTime.TryParse(product.DeliveryDate.Value, out var deliveryDate)
+                    && deliveryDate.Date < today)
+                {
+                    var newDeliveryDate = today.AddDays(2).ToString("yyyy-MM-dd");
+                    var updatedDeliveryDate = product.DeliveryDate with { Value = newDeliveryDate };
+
+                    return product with { DeliveryDate = updatedDeliveryDate };
+                }
+
+                return product;
+            })
+            .ToList();
+
+        if (updatedProducts is null)
+            return payload;
+
+        return payload with { Products = updatedProducts };
+    }
+}
