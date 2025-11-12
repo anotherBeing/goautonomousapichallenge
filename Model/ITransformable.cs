13287d0e9dd634f2262173b5250b6a804bfbc29f
Jonatan Kornhill <jonakor@dfds.com>
2025-11-12 21:16:04 +0100

    Add interface ITransformable

---
+++ Model/ITransformable.cs
@@ -0,0 +1,7 @@
+﻿using TransformApi.Transformations;
+
+namespace TransformApi.Model;
+public interface ITransformable<T> where T : ITransformable<T>
+{
+    T TransformBy(T payload, IEnumerable<ITransformation<T>> transformations);
+}
