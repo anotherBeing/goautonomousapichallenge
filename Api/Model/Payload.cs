4897d61d30d5b77e34a8813fa53e0dcd1dd91a71
Jonatan Kornhill <jonakor@dfds.com>
2025-11-15 12:51:15 +0100

    Move API to seprate project

---
+++ Api/Model/Payload.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api.Model;
+
+public record Payload
+{
+    [JsonPropertyName("mail_id")]
+    public Guid? MailId { get; init; }
+    [JsonPropertyName("iteration")]
+    public string? Iteration { get; init; }
+    [JsonPropertyName("intent")]
+    public string? Intent { get; init; }
+    [JsonPropertyName("area")]
+    public Area? Area { get; init; }
+    [JsonPropertyName("products")]
+    public List<Product>? Products { get; init; }
+    [JsonPropertyName("header")]
+    public Header? Header { get; init; }
+}
+
+public record Area
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; init; }
+}
+
+public record Product
+{
+    [JsonPropertyName("line_number")]
+    public int? LineNumber { get; init; }
+    [JsonPropertyName("product_number")]
+    public ProductField? ProductNumber { get; init; }
+    [JsonPropertyName("unit_price")]
+    public ProductField? UnitPrice { get; init; }
+    [JsonPropertyName("delivery_date")]
+    public ProductField? DeliveryDate { get; init; }
+}
+
+public record ProductField
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; init; }
+    [JsonPropertyName("value")]
+    public string? Value { get; init; }
+}
+
+public record Header
+{
+    [JsonPropertyName("order_number")]
+    public ProductField? OrderNumber { get; init; }
+    [JsonPropertyName("account_id")]
+    public ProductField? AccountId { get; init; }
+}
