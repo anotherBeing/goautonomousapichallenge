ff0ec9f4090c2b137732c6d8c5349fc3c1e47c09
Jonatan Kornhill <jonakor@dfds.com>
2025-11-15 12:19:10 +0100

    Implement IterationRefreshSwedenToSanitizeProductNumbers

--- Transformations/IterationRefreshSwedenToSanitizeProductNumbers.cs
+++ Transformations/IterationRefreshSwedenToSanitizeProductNumbers.cs
@@ -1,10 +1,28 @@
-﻿using TransformApi.Model;
+﻿using System.Text.RegularExpressions;
+using TransformApi.Model;
 
 namespace TransformApi.Transformations;
 public class IterationRefreshSwedenToSanitizeProductNumbers : ITransformation<Payload>
 {
+    private const string SanitizePattern = "[^0-9]";
+    private const string TransformationArea = "SE1";
+    private const string TransformationIteration = "Refresh";
+
     public Payload Transform(Payload payload)
     {
-        throw new NotImplementedException();
+        if (payload.Area?.Name != TransformationArea || payload.Iteration != TransformationIteration)
+        {
+            return payload;
+        }
+
+        var sanitizedProducts = payload.Products?.Select(p => p with
+        {
+            ProductNumber = p.ProductNumber with
+            {
+                Value = Regex.Replace(p.ProductNumber.Value, SanitizePattern, "")
+            }
+        }).ToList();
+
+        return payload with { Products = sanitizedProducts };
     }
 }
