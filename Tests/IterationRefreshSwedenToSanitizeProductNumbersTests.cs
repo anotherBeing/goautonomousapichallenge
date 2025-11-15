f8b2cd2665186ec05208f05488f6e4ce65775393
Jonatan Kornhill <jonakor@dfds.com>
2025-11-15 12:16:07 +0100

    add tests for transformation IterationRefreshSwedenToSanitizeProductNumbers

---
+++ Tests/IterationRefreshSwedenToSanitizeProductNumbersTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TransformApi.Model;
+using TransformApi.Transformations;
+using Xunit;
+
+namespace Tests;
+public class IterationRefreshSwedenToSanitizeProductNumbersTests : TransformationTestsBase
+{
+    [Fact]
+    public void GivenIterationRefresh_WhenAreaSe1_ThenSanitizeProductNumbers()
+    {
+        var payload = GetPayload();
+        payload = payload with { Area = new Area { Name = "SE1" }, Iteration = "Refresh" };
+
+        var transformation = new IterationRefreshSwedenToSanitizeProductNumbers();
+
+        var transformedPayload = transformation.Transform(payload);
+
+        var expectedProducts = payload.Products?.Select(p => p with
+        {
+            ProductNumber = p.ProductNumber with
+            {
+                Value = Regex.Replace(p.ProductNumber.Value, "[^0-9]", "")
+            }
+        });
+
+        Assert.NotEqual(payload, transformedPayload);
+        Assert.Equal(expectedProducts, transformedPayload.Products);
+    }
+
+    [Fact]
+    public void GivenIterationRefresh_WhenAreaNotSe1_ThenNoTransformation()
+    {
+        var payload = GetPayload();
+        payload = payload with { Area = new Area { Name = "US1" }, Iteration = "Refresh" };
+
+        var transformation = new IterationRefreshSwedenToSanitizeProductNumbers();
+
+        var transformedPayload = transformation.Transform(payload);
+
+        Assert.Equal(payload, transformedPayload);
+    }
+
+    [Fact]
+    public void GivenIterationNotRefresh_WhenAreaSe1_ThenNoTransformation()
+    {
+            var payload = GetPayload();
+        payload = payload with { Area = new Area { Name = "SE1" }, Iteration = "NotRefresh" };
+
+        var transformation = new IterationRefreshSwedenToSanitizeProductNumbers();
+
+        var transformedPayload = transformation.Transform(payload);
+
+        Assert.Equal(payload, transformedPayload);
+    }
+
+    [Fact]
+    public void GivenIterationNotRefresh_WhenAreaNotSe1_ThenNoTransformation()
+    {
+        var payload = GetPayload();
+        payload = payload with { Area = new Area { Name = "US1" }, Iteration = "NotRefresh" };
+
+        var transformation = new IterationRefreshSwedenToSanitizeProductNumbers();
+
+        var transformedPayload = transformation.Transform(payload);
+
+        Assert.Equal(payload, transformedPayload);
+    }
+}
