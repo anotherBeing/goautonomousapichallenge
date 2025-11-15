8625020470c2b6b3beb1c8d75a2fdc7a201e2ac0
Jonatan Kornhill <jonakor@dfds.com>
2025-11-15 13:22:57 +0100

    Add payload class for API

---
+++ API/Model/Payload.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Transform.API.Model;
+
+public class Payload
+{
+    [JsonPropertyName("mail_id")]
+    public Guid? MailId { get; set; }
+
+    [JsonPropertyName("iteration")]
+    public string? Iteration { get; set; }
+
+    [JsonPropertyName("intent")]
+    public string? Intent { get; set; }
+
+    [JsonPropertyName("area")]
+    public Area? Area { get; set; }
+
+    [JsonPropertyName("products")]
+    public List<Product>? Products { get; set; }
+
+    [JsonPropertyName("header")]
+    public Header? Header { get; set; }
+}
+
+public class Area
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
+
+public class Product
+{
+    [JsonPropertyName("line_number")]
+    public int? LineNumber { get; set; }
+
+    [JsonPropertyName("product_number")]
+    public ProductField? ProductNumber { get; set; }
+
+    [JsonPropertyName("unit_price")]
+    public ProductField? UnitPrice { get; set; }
+
+    [JsonPropertyName("delivery_date")]
+    public ProductField? DeliveryDate { get; set; }
+}
+
+public class ProductField
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("value")]
+    public string? Value { get; set; }
+}
+
+public class Header
+{
+    [JsonPropertyName("order_number")]
+    public ProductField? OrderNumber { get; set; }
+
+    [JsonPropertyName("account_id")]
+    public ProductField? AccountId { get; set; }
+}
--- Model/Payload.cs
+++ Model/Payload.cs
@@ -1,22 +1,15 @@
-using System.Text.Json.Serialization;
 using TransformApi.Transformations;
 
 namespace TransformApi.Model;
 
 public record Payload : ITransformable<Payload>
 {
-    [JsonPropertyName("mail_id")]
-    public Guid? MailId { get; init; }
-    [JsonPropertyName("iteration")]
-    public string? Iteration { get; init; }
-    [JsonPropertyName("intent")]
-    public string? Intent { get; init; }
-    [JsonPropertyName("area")]
-    public Area? Area { get; init; }
-    [JsonPropertyName("products")]
-    public List<Product>? Products { get; init; }
-    [JsonPropertyName("header")]
-    public Header? Header { get; init; }
+    public Guid MailId { get; init; }
+    public string Iteration { get; init; }
+    public string Intent { get; init; }
+    public Area Area { get; init; }
+    public List<Product> Products { get; init; }
+    public Header Header { get; init; }
     public Payload TransformBy(Payload payload, IEnumerable<ITransformation<Payload>> transformations)
     {
         return transformations.Aggregate(payload, (current, transformation) => transformation.Transform(current));
@@ -25,34 +18,25 @@ public record Payload : ITransformable<Payload>
 
 public record Area
 {
-    [JsonPropertyName("name")]
-    public string? Name { get; init; }
+    public string Name { get; init; }
 }
 
 public record Product
 {
-    [JsonPropertyName("line_number")]
-    public int? LineNumber { get; init; }
-    [JsonPropertyName("product_number")]
-    public ProductField? ProductNumber { get; init; }
-    [JsonPropertyName("unit_price")]
-    public ProductField? UnitPrice { get; init; }
-    [JsonPropertyName("delivery_date")]
-    public ProductField? DeliveryDate { get; init; }
+    public int LineNumber { get; init; }
+    public ProductField ProductNumber { get; init; }
+    public ProductField UnitPrice { get; init; }
+    public ProductField DeliveryDate { get; init; }
 }
 
 public record ProductField
 {
-    [JsonPropertyName("id")]
-    public string? Id { get; init; }
-    [JsonPropertyName("value")]
-    public string? Value { get; init; }
+    public string Id { get; init; }
+    public string Value { get; init; }
 }
 
 public record Header
 {
-    [JsonPropertyName("order_number")]
-    public ProductField? OrderNumber { get; init; }
-    [JsonPropertyName("account_id")]
-    public ProductField? AccountId { get; init; }
+    public ProductField OrderNumber { get; init; }
+    public ProductField AccountId { get; init; }
 }
