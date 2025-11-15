8d81497a3d33d0c99a9257d9dd7d66e4addc9f5e
Jonatan Kornhill <jonakor@dfds.com>
2025-11-15 11:29:22 +0100

    Add new IntentRequestSimpleQuoteToAddNewProduct with tests but no implementation

---
+++ Tests/IntentRequestSimpleQuoteToAddNewProductTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransformApi.Model;
+using TransformApi.Transformations;
+using Xunit;
+
+namespace Tests;
+public class IntentRequestSimpleQuoteToAddNewProductTests : TransformationTestsBase
+{
+    [Fact]
+    public void GivenIntentRequestSimple_ThenNewProductShouldBeAdded()
+    {
+        var payload = GetPayload();
+
+        payload = payload with { Intent = "request_simple_order" };
+
+        var lastProduct = payload.Products.Last();
+
+        var expectedAddedProduct = new Product()
+        {
+            LineNumber = lastProduct.LineNumber + 1,
+            ProductNumber = new ProductField() { Value = "FREIGHT" },
+            UnitPrice = new ProductField() { Value = "50" },
+        };
+
+        var transformer = new IntentRequestSimpleQuoteToAddNewProduct();
+
+        var result = transformer.Transform(payload);
+
+        Assert.True(payload.Products.All(item => result.Products.Contains(item)));
+        Assert.Contains(expectedAddedProduct, result.Products);
+    }
+}
---
+++ Transformations/IntentRequestSimpleQuoteToAddNewProduct.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlTypes;
+using TransformApi.Model;
+
+namespace TransformApi.Transformations;
+public class IntentRequestSimpleQuoteToAddNewProduct : ITransformation<Payload>
+{
+    private const string TransformationTrigger = "request_simple_quote";
+
+    public Payload Transform(Payload payload)
+    {
+        if (payload.Intent is null)
+            return payload;
+
+        var shouldTransform = payload.Intent.Equals(TransformationTrigger);
+
+        if (shouldTransform)
+        {
+            
+        }
+
+        return payload;
+    }
+}
